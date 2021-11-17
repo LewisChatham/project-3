bac8af8f8560c0ee9e6f8c9a11c1b2e2c1fa0cb4
Kevin98 <kevinryner@yahoo.co.uk>
2021-11-17 20:47:29 +0000

    add and remove wishlists resolvers added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,23 +1,26 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { Wishlist, User, Gift } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { Wishlist, User, Gift } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     wishlist: async (parent, { wishlistId }) => {
-      return User.find({_id: wishlistId}).populate('wishlists');
+      return User.find({ _id: wishlistId }).populate("wishlists");
     },
     me: async (parent, { id }, context) => {
-        const foundUser = await User.findOne({
-            $or: [{ _id: context.user ? context.user._id : id }, { username: context.user.username }],
-          }).populate("Wishlists");
-      
-          if (!foundUser) {
-            throw new AuthenticationError('You need to be logged in!');
-          }
+      const foundUser = await User.findOne({
+        $or: [
+          { _id: context.user ? context.user._id : id },
+          { username: context.user.username },
+        ],
+      }).populate("Wishlists");
 
-          return foundUser;
-      },
+      if (!foundUser) {
+        throw new AuthenticationError("You need to be logged in!");
+      }
+
+      return foundUser;
+    },
   },
 
   Mutation: {
@@ -30,20 +33,49 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('No user found with this email address');
+        throw new AuthenticationError("No user found with this email address");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
 
       return { token, user };
     },
-   
+    addWishlist: async (parent, { listName, priceLimit }, context) => {
+      if (context.user) {
+        const wishlist = await Wishlist.create({
+          listName,
+          priceLimit,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { wishlists: wishlist._id } }
+        );
+
+        return wishlist;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    removeWishlist: async (parent, { wishlistId }, context) => {
+      if (context.user) {
+        const wishlist = await Wishlist.findOneAndDelete({
+          _id: wishlistId,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { wishlists: wishlist._id } }
+        );
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
 };
 
