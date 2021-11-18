456368b9a976742f31988b389fea6160c5a34944
Kevin98 <kevinryner@yahoo.co.uk>
2021-11-18 20:38:09 +0000

    index and app files in client updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 // First we import our Hello component from our components folder.
 import Hello from './components/Hello';
 
 // React apps typically have a single App component at the very top that can reference other React components.
 // This component, `App`, is our main component that is importing `Hello` and rendering it in the return method.
+
+
+
+
+
 function App() {
-  return <Hello />;
+  return ( 
+    <ApolloProvider client={client}>
+      <Hello />;
+    </ApolloProvider>
+  );
 }
 
 export default App;
