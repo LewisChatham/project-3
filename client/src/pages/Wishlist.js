ca3376e0d97ad911b9e837783ed8943e2f3db9b8
Kevin98 <kevinryner@yahoo.co.uk>
2021-11-25 20:59:22 +0000

    updated home and wishlist comps

--- client/src/pages/Wishlist.js
+++ client/src/pages/Wishlist.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import GiftForm from '../components/GiftForm'
 import {QUERY_WISHLIST} from '../utils/queries'
 
@@ -19,30 +19,12 @@ const Wishlist = () => {
     variables: { wishlistId: wishlistId },
   });
 
-  // const [ removeGift, { error } ] = useMutation(REMOVE_GIFT)
-
   const [updateGift] = useMutation(UPDATE_GIFT);
 
   const wishlist = data?.wishlist || {};
 
   const giftList = wishlist?.gifts || [];
 
-
-
-  // const handleGiftDelete = async (giftId) => {
-  //     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //     if (!token) {
-  //       return false;
-  //     }
-
-  //     try {
-  //     const { data } = await removeGift({variables: {wishlistId, giftId}});
-  //     console.log(data);
-  //     } catch(err){
-  //         console.error(err)
-  //     }
-  // }
 
   const handleGiftUpdate = async (giftId, itemBought) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
